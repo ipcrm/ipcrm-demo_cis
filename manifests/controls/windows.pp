5f9d46f444b6ae55e3106178c5f13db032717dbc
ipcrm <mcadorette@gmail.com>
2016-09-28 13:22:46 -0400

    Including all controls

--- manifests/controls/windows.pp
+++ manifests/controls/windows.pp
@@ -2,6 +2,14 @@ class demo_cis::controls::windows {
   case $::operatingsystemmajrelease {
     '2012 R2': {
       include ::demo_cis::controls::windows::server2012r2::cis_1_1_1
+      include ::demo_cis::controls::windows::server2012r2::cis_1_1_2
+      include ::demo_cis::controls::windows::server2012r2::cis_1_1_3
+      include ::demo_cis::controls::windows::server2012r2::cis_1_1_4
+      include ::demo_cis::controls::windows::server2012r2::cis_1_1_5
+      include ::demo_cis::controls::windows::server2012r2::cis_1_1_6
+      include ::demo_cis::controls::windows::server2012r2::cis_9_1_1
+      include ::demo_cis::controls::windows::server2012r2::cis_9_2_1
+      include ::demo_cis::controls::windows::server2012r2::cis_9_3_1
     }
     default: {}
   }
