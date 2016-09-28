cf71d24883b99a96f7c639f5b439aed5f2c4685e
ipcrm <mcadorette@gmail.com>
2016-09-28 14:32:50 -0400

    fixing class name

--- manifests/controls/windows/server2012r2/cis_helpers.pp
+++ manifests/controls/windows/server2012r2/cis_helpers.pp
@@ -1,4 +1,4 @@
-class demo_cis::controls::windows::server2012r2::cis_1_1_1 {
+class demo_cis::controls::windows::server2012r2::cis_helpers {
 
   service{'cis-windows_firewall':
     ensure => running,
