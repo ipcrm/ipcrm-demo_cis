dee17aa1cd6619290ed6657d8f70dbfcafbb3b08
ipcrm <mcadorette@gmail.com>
2017-06-20 07:17:01 -0400

    typo

--- manifests/controls/windows/server2012r2/cis_helpers.pp
+++ manifests/controls/windows/server2012r2/cis_helpers.pp
@@ -2,7 +2,7 @@ class demo_cis::controls::windows::server2012r2::cis_helpers(
   $create_service_helper = false,
 ){
 
-  if create_service_helper == true {
+  if $create_service_helper == true {
     service{'windows_firewall':
       ensure => running,
       name   => 'MpsSvc',
