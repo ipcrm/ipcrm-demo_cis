7ec3a6f7bfdb50198ba0c5c8703ff583e87b517c
ipcrm <mcadorette@gmail.com>
2017-06-20 07:05:43 -0400

    only manage motd if not managed

--- manifests/controls/redhat/seven/cis_1_7_1_1.pp
+++ manifests/controls/redhat/seven/cis_1_7_1_1.pp
@@ -2,13 +2,15 @@ class demo_cis::controls::redhat::seven::cis_1_7_1_1 (
   $enable = true
 ){
   if $enable == true {
-    file {'cis_1_7_1_1_motd':
-      ensure  => present,
-      path    => '/etc/motd',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => 'Authorized uses only. All activity may be monitored and reported.',
+    if !defined(File['/etc/motd']) {
+      file {'cis_1_7_1_1_motd':
+        ensure  => present,
+        path    => '/etc/motd',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => 'Authorized uses only. All activity may be monitored and reported.',
+      }
     }
   }
 
