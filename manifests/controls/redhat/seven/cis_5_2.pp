9d7c21085793fb064cf47ef546f52819283530f9
ipcrm <mcadorette@gmail.com>
2017-08-23 14:20:08 -0400

    updates for new module

--- manifests/controls/redhat/seven/cis_5_2.pp
+++ manifests/controls/redhat/seven/cis_5_2.pp
@@ -3,19 +3,13 @@ class demo_cis::controls::redhat::seven::cis_5_2 (
 ){
   if $enable == true {
     class {'::ssh::server':
-      ssh_config_owner                  => 'root',
-      ssh_config_group                  => 'root',
-      ssh_config_mode                   => '0600',
-      sshd_config_loglevel              => 'INFO',
-      sshd_x11_forwarding               => 'no',
-      sshd_config_maxauthtries          => 4,
-      sshd_ignorerhosts                 => 'yes',
-      sshd_hostbasedauthentication      => 'no',
-      permit_root_login                 => 'no',
-      sshd_config_permitemptypasswords  => 'no',
-      sshd_config_permituserenvironment => 'no',
-      sshd_config_ciphers               => ['aes256-ctr','aes192-ctr','aes128-ctr'],
-      sshd_config_macs                  => [
+      syslog_level           => 'INFO',
+      x11_forwarding         => 'no',
+      max_auth_retries       => 4,
+      sshd_ignorerhosts      => 'yes',
+      permit_root_login      => 'no',
+      ciphers                => ['aes256-ctr','aes192-ctr','aes128-ctr'],
+      macs                   => [
           'hmac-sha2-512-etm@openssh.com',
           'hmac-sha2-256-etm@openssh.com',
           'umac-128-etm@openssh.com',
@@ -23,11 +17,10 @@ class demo_cis::controls::redhat::seven::cis_5_2 (
           'hmac-sha2-256',
           'umac-128@openssh.com',
       ],
-      sshd_client_alive_interval        => '300',
-      sshd_client_alive_count_max       => '0',
-      sshd_config_login_grace_time      => '60',
-      sshd_config_allowgroups           => ['wheel'],
-      sshd_config_banner                => '/etc/issue.net',
+      client_alive_interval  => '300',
+      client_alive_count_max => '0',
+      config_allowed_groups  => ['wheel'],
+      banner_file            => '/etc/issue.net',
     }
   }
 }
