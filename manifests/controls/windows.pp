b1648637017ad124380a63788649783d949a19db
gbrandyb <42916741+gbrandyb@users.noreply.github.com>
2020-04-07 09:40:58 -0400

    enable all controls

--- manifests/controls/windows.pp
+++ manifests/controls/windows.pp
@@ -3,19 +3,19 @@ class demo_cis::controls::windows {
     '2012 R2', '2016': {
       include ::demo_cis::controls::windows::server2012r2::cis_helpers
       include ::demo_cis::controls::windows::server2012r2::cis_1_1_1
-      # include ::demo_cis::controls::windows::server2012r2::cis_1_1_2
-      # include ::demo_cis::controls::windows::server2012r2::cis_1_1_3
-      # include ::demo_cis::controls::windows::server2012r2::cis_1_1_4
-      # include ::demo_cis::controls::windows::server2012r2::cis_1_1_5
-      # include ::demo_cis::controls::windows::server2012r2::cis_1_1_6
-      # include ::demo_cis::controls::windows::server2012r2::cis_9_1_1
-      # include ::demo_cis::controls::windows::server2012r2::cis_9_2_1
-      # include ::demo_cis::controls::windows::server2012r2::cis_9_3_1
-      # include ::demo_cis::controls::windows::server2012r2::cis_17_1_1
-      # include ::demo_cis::controls::windows::server2012r2::cis_17_2_1
-      # include ::demo_cis::controls::windows::server2012r2::cis_17_3_1
-      # include ::demo_cis::controls::windows::server2012r2::cis_17_4_1
-      # include ::demo_cis::controls::windows::server2012r2::cis_17_5_1
+      include ::demo_cis::controls::windows::server2012r2::cis_1_1_2
+      include ::demo_cis::controls::windows::server2012r2::cis_1_1_3
+      include ::demo_cis::controls::windows::server2012r2::cis_1_1_4
+      include ::demo_cis::controls::windows::server2012r2::cis_1_1_5
+      include ::demo_cis::controls::windows::server2012r2::cis_1_1_6
+      include ::demo_cis::controls::windows::server2012r2::cis_9_1_1
+      include ::demo_cis::controls::windows::server2012r2::cis_9_2_1
+      include ::demo_cis::controls::windows::server2012r2::cis_9_3_1
+      include ::demo_cis::controls::windows::server2012r2::cis_17_1_1
+      include ::demo_cis::controls::windows::server2012r2::cis_17_2_1
+      include ::demo_cis::controls::windows::server2012r2::cis_17_3_1
+      include ::demo_cis::controls::windows::server2012r2::cis_17_4_1
+      include ::demo_cis::controls::windows::server2012r2::cis_17_5_1
     }
     default: {}
   }
